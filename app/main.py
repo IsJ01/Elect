cd3d20e6a957297a1d27c3cb6bafabc13776cb9a
IsJ01 <islamishtalbagamaev27@gmail.com>
2025-10-30 00:11:57 +0300

    new file:   app/__pycache__/main.cpython-313.pyc 	new file:   app/__pycache__/schemas.cpython-313.pyc 	modified:   app/__pycache__/services.cpython-313.pyc 	modified:   app/main.py 	modified:   app/schemas.py 	modified:   app/services.py 	modified:   requirements.txt

--- app/main.py
+++ app/main.py
@@ -3,17 +3,33 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordBearer
 
-load_dotenv("../.env")
+from app.services import UserService
+
+from app.database import get_db
+
+load_dotenv()
 
 SIGNING_KEY = os.getenv("SIGNING_KEY")
+
 ACCESS_TOKEN_EXPIRE_HOURS = 3
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
+@app.on_event("startup")
+def startup_event():
 
+    db = next(get_db())
+    user_service = UserService()
+
+    try:
+        user_service.ensure_superuser_exists(db)
+    except Exception as e:
+        print(f"❌ Error during startup: {e}")
+    finally:
+        db.close()
 
 
--- app/services.py
+++ app/services.py
@@ -1,17 +1,80 @@
+import os
 from datetime import timezone, datetime, timedelta
 
 import jwt
+from sqlalchemy.orm import Session
 
-from models import User
+from app.schemas import UserCreateDto
+from .models import User
+from dotenv import load_dotenv
 
-class JwtService:
 
-    def generate_token(self, user: User, signing_key: str, hours: int):
-        claims = {
-            "username": user.username,
-            "is_superuser": user.is_superuser,
-        }
-        expire = datetime.now(timezone.utc) + timedelta(hours=15)
-        claims["exp"] = expire
-        encoded_jwt = jwt.encode(claims, signing_key, algorithm="HS256")
-        return encoded_jwt
+def generate_token(user: User, signing_key: str, hours: int):
+    claims = {
+        "username": user.username,
+        "is_superuser": user.is_superuser,
+    }
+    expire = datetime.now(timezone.utc) + timedelta(hours=15)
+    claims["exp"] = expire
+    encoded_jwt = jwt.encode(claims, signing_key, algorithm="HS256")
+    return encoded_jwt
+
+
+class UserService:
+    @staticmethod
+    def get_by_username(db: Session, username: str):
+        return db.query(User).filter(User.username == username).first()
+
+    @staticmethod
+    def get_by_email(db: Session, email: str):
+        return db.query(User).filter(User.email == email).first()
+
+    def create_user(self, db: Session, user_data: UserCreateDto, is_superuser: bool = False):
+
+        if self.get_by_username(db, user_data.username):
+            raise ValueError("User with this username already exists")
+
+        db_user = User(
+            username=user_data.username,
+            is_superuser=is_superuser
+        )
+        db_user.set_password(user_data.password)
+
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+
+    def create_superuser(self, db: Session, username: str, password: str):
+        user_data = UserCreateDto(
+            username=username,
+            password=password,
+        )
+        return self.create_user(db, user_data, is_superuser=True)
+
+    @staticmethod
+    def get_superusers_count(db: Session) -> int:
+        return db.query(User).filter(User.is_superuser == True).count()
+
+    def ensure_superuser_exists(self, db: Session):
+        if self.get_superusers_count(db) > 0:
+            return
+
+        username = os.getenv("ADMIN_NAME")
+        password = os.getenv("ADMIN_PASS")
+
+        if self.get_by_username(db, username):
+            user = self.get_by_username(db, username)
+            user.is_superuser = True
+            db.commit()
+            return
+
+        try:
+            self.create_superuser(
+                db=db,
+                username=username,
+                password=password
+            )
+
+        except Exception as e:
+            print(f"❌ Error creating superuser: {e}")
